29a0228bfa8824151a27da09eb93d7ab6709da9a
somesh <qasomesh001@gmail.com>
2024-10-22 23:14:18 +0530

    Initial commit

---
+++ src/test/java/Temp_pack/timestamp.java
@@ -0,0 +1,33 @@
+package Temp_pack;
+
+import java.util.Date;
+import java.util.Random;
+
+import org.testng.annotations.Test;
+
+import io.opentelemetry.sdk.metrics.data.Data;
+
+public class timestamp {
+	
+	@Test
+	public String reusableemail()
+	{
+	//	Date date = new Date();
+	//	System.out.println(date);
+		
+	return new  Date().toString().replace(" ", "").replace(":", "") + "@gmail.com";
+	//	System.out.println(emailwithoutspace);
+		
+		//String email = emailwithoutspace + "@gmail.com";
+	//	System.out.println(email);
+		//return email;
+	}
+
+	
+ @Test
+ public String resuablemobile()
+ {
+	 return "9" + (100000000L + (long) (new Random().nextDouble() * 900000000L));
+
+ }
+}
---
+++ src/test/java/Test_PACK/TC_RF_003.java
@@ -0,0 +1,90 @@
+package Test_PACK;
+
+import static org.testng.Assert.assertEquals;
+
+import java.time.Duration;
+import java.util.Date;
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TC_RF_003 {
+
+    WebDriver driver;
+    
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get("https://tutorialsninja.com/demo/");
+    }
+    
+    public static String reusableEmail() {
+        return new Date().toString().replace(" ", "").replace(":", "") + "@gmail.com";
+    }
+    
+    public static String reusableMobile() {
+        return "9" + (100000000L + (long) (new Random().nextDouble() * 900000000L));
+    }
+    
+    @Test
+    public void verifyRegisteringAnAccount() {
+        // Navigating to Register
+        driver.findElement(By.xpath("//span[normalize-space()='My Account']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Register']")).click();
+        
+        // Entering user details
+        driver.findElement(By.id("input-firstname")).sendKeys("qa");
+        driver.findElement(By.id("input-lastname")).sendKeys("qa");
+        
+        String email = reusableEmail();
+        String mobile = reusableMobile();
+        
+        driver.findElement(By.id("input-email")).sendKeys(email);
+        driver.findElement(By.id("input-telephone")).sendKeys(mobile);
+        
+        System.out.println("Email: " + email);
+        System.out.println("Mobile: " + mobile);
+        
+        driver.findElement(By.id("input-password")).sendKeys("Test@123");
+        driver.findElement(By.id("input-confirm")).sendKeys("Test@123");
+        
+        // Submitting form
+        driver.findElement(By.xpath("(//input[@value='1'])[2]")).click();
+        driver.findElement(By.xpath("//input[@name='agree']")).click();
+        driver.findElement(By.xpath("//input[@value='Continue']")).click();
+        
+        // Verifying success message
+        WebElement successMessage = driver.findElement(By.xpath("//h1[normalize-space()='Your Account Has Been Created!']"));
+        String actualSuccessText = successMessage.getText();
+        String expectedSuccessText = "Your Account Has Been Created!";
+        Assert.assertEquals(actualSuccessText, expectedSuccessText);
+        
+        // Verifying Logout option
+        boolean isLogoutDisplayed = driver.findElement(By.xpath("//a[@class='list-group-item'][normalize-space()='Logout']")).isDisplayed();
+        System.out.println("Is Logout Displayed: " + isLogoutDisplayed);
+        Assert.assertTrue(isLogoutDisplayed);
+    }
+    
+    @AfterMethod
+    public void tearDown() {
+        // Close the browser after the test
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
+
+	
+
+	
+	
+
