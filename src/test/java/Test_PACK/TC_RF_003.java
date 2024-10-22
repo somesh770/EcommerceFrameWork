b68f65b5d12e3395101eab83a903684f851da255
somesh <qasomesh001@gmail.com>
2024-10-22 23:28:36 +0530

    created before and after class

--- src/test/java/Test_PACK/TC_RF_003.java
+++ src/test/java/Test_PACK/TC_RF_003.java
@@ -15,18 +15,9 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class TC_RF_003 {
-
-    WebDriver driver;
-    
-    @BeforeMethod
-    public void setUp() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-        driver.get("https://tutorialsninja.com/demo/");
-    }
-    
+public class TC_RF_003 extends Base_Class
+{
+       
     public static String reusableEmail() {
         return new Date().toString().replace(" ", "").replace(":", "") + "@gmail.com";
     }
@@ -74,13 +65,7 @@ public class TC_RF_003 {
         Assert.assertTrue(isLogoutDisplayed);
     }
     
-    @AfterMethod
-    public void tearDown() {
-        // Close the browser after the test
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+    
 }
 
 	
---
+++ src/test/java/Test_PACK/TC_RF_004.java
@@ -0,0 +1,6 @@
+package Test_PACK;
+
+public class TC_RF_004 
+{
+	
+}
