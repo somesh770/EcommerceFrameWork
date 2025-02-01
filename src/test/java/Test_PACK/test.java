5354457d5c951d2f8c8c07ef3e2af7758d9424fa
Somesh <qasomesh001@gmail.com>
2025-02-01 18:17:17 +0530

    added dummy test class

---
+++ src/test/java/Test_PACK/test.java
@@ -0,0 +1,14 @@
+package Test_PACK;
+
+import org.testng.annotations.Test;
+
+public class test {
+
+    @Test
+    public  void test()
+
+    {
+        System.out.println("hellow world");
+    }
+
+}
